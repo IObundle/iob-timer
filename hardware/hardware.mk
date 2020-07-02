7792ba13e1d3230624fd9970a6e0e4e66de85519
Chris Edgley <christopher@iobundle.com>
2020-07-02 16:35:43 +0100

    Updated includes

--- hardware/hardware.mk
+++ hardware/hardware.mk
@@ -1,5 +1,12 @@
 TIMER_HW_DIR:=$(TIMER_DIR)/hardware
 
+#include
+TIMER_INC_DIR:=$(TIMER_HW_DIR)/include
+INCLUDE+=$(incdir) $(TIMER_INC_DIR)
+
+#headers
+VHDR+=$(wildcard $(TIMER_INC_DIR)/*.vh)
+
 #sources
 TIMER_SRC_DIR:=$(TIMER_DIR)/hardware/src
-VSRC+=$(TIMER_HW_DIR)/src/*.v
+VSRC+=$(wildcard $(TIMER_HW_DIR)/src/*.v)
