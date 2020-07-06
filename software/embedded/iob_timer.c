b2209fc9d8906315fc8888ad350562119c2339db
Chris Edgley <christopher@iobundle.com>
2020-07-06 18:19:20 +0100

    file name conforming, fixed timer functions, added get timer in ms and s

--- software/embedded/iob_timer.c
+++ software/embedded/iob_timer.c
@@ -13,18 +13,53 @@ unsigned long long timer_get_count(int base) {
   MEMGET(base, TIMER_STOP);
   timer_high = (unsigned int) MEMGET(base, TIMER_DATA_HIGH);
   timer_low = (unsigned int) MEMGET(base, TIMER_DATA_LOW);
-  timer_total = ((unsigned long long) timer_high) << 32;
+  timer_total = timer_high;
+  timer_total = timer_total << 32;
   timer_total |= timer_low;
   
-  return (timer_total);
+  return timer_total;
 } 
 
+//deprecated
+//unsigned int timer_get_count_us(int base) {return 0;}
+
 //counter with 10us resolution and max value of almost 12 hours
-unsigned int timer_get_count_us(int base) {
-  unsigned int timer_high, timer_low, timer;
+unsigned int timer_time_us(int base) {
+  unsigned int timer_high, timer_low, timer, calc; //, aux;
+  //  unsigned long long timer_total;
   MEMGET(base, TIMER_STOP);
   timer_high = (unsigned int) MEMGET(base, TIMER_DATA_HIGH);
   timer_low = (unsigned int) MEMGET(base, TIMER_DATA_LOW);
-  timer = (timer_high << 22) | (timer_low >> 10);
-  return (timer*((1024*1000000)/FREQ));
+  //  timer = (timer_high << 22) | (timer_low >> 10); // (=) /1024
+
+  calc = FREQ/1000; //freq in kHz //ideally FREQ/1000000
+  calc /= 1000; //freq in MHz
+  timer = timer_low/calc;         //ideally timer_total
+  return timer;
+}
+
+//counter with 10ms resolution and unreachable max value
+unsigned int timer_time_ms(int base) {
+  unsigned int timer_high, timer_low, timer, calc;
+  MEMGET(base, TIMER_STOP);
+  timer_high = (unsigned int) MEMGET(base, TIMER_DATA_HIGH);
+  timer_low = (unsigned int) MEMGET(base, TIMER_DATA_LOW);
+  //  timer = (timer_high << 12) | (timer_low >> 20); // (=) /(1024*1024)
+
+  calc = FREQ/1000;
+  //  calc /= 1000;
+  timer = timer_low/calc; //ideally timer_total
+  return timer;
+}
+
+//counter with 10s resolution and unreachable max value
+unsigned int timer_time_s(int base) {
+  unsigned int timer_high, timer_low, timer, calc;
+  MEMGET(base, TIMER_STOP);
+  timer_high = (unsigned int) MEMGET(base, TIMER_DATA_HIGH);
+  timer_low = (unsigned int) MEMGET(base, TIMER_DATA_LOW);
+  //  timer = (timer_high << 12) | (timer_low >> 20);
+  calc = FREQ;
+  timer = timer_low/calc; //ideally timer_total
+  return timer;
 }
--- software/iob_timer.h
+++ software/iob_timer.h
@@ -1,6 +1,7 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include <time.h>
 
 #ifndef MEMSET
 #define MEMSET(base, location, value) (*((volatile int*) (base + (sizeof(int)) * location)) = value)
@@ -22,5 +23,8 @@ void timer_reset(int base);
 //Gets the current number of cycles since reset
 unsigned long long timer_get_count(int base);
 unsigned int timer_get_count_us(int base);
+unsigned int timer_time_us(int base);
+unsigned int timer_time_ms(int base);
+unsigned int timer_time_s(int base);
 
 #endif
