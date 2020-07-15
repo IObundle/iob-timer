823d10fb070e9f6b3a311bcb806ce6376d1f308e
P_Miranda <pedro_miranda2@hotmail.com>
2020-07-15 12:04:07 +0100

    Fixes to Timer
    Fixed hardware.mk for vivado synthesis
    
    Improved driver counter range
    
    Added pc simulation option

--- software/pc/iob_timer.c
+++ software/pc/iob_timer.c
@@ -27,3 +27,37 @@ unsigned int timer_get_count_us(int base) {
   //return time in us
   return (unsigned long long) (time_dif_secs * 1000000);
 }
+
+
+//counter with 1us resolution and max value of 1h11
+unsigned int timer_time_us(int base) {
+
+  // get current time
+  end  = clock();
+  // get difference in seconds
+  double time_dif_secs = (double) (end - start) / CLOCKS_PER_SEC;
+  //return time in us
+  return (unsigned int) (time_dif_secs * 1000000);
+}
+
+//counter with 1ms resolution and max value of over 49 days
+unsigned int timer_time_ms(int base) {
+
+  // get current time
+  end  = clock();
+  // get difference in seconds
+  double time_dif_secs = (double) (end - start) / CLOCKS_PER_SEC;
+  //return time in ms
+  return (unsigned int) (time_dif_secs * 1000);
+}
+
+//counter with 1s resolution and max value of over 136 years
+unsigned int timer_time_s(int base) {
+
+  // get current time
+  end  = clock();
+  // get difference in seconds
+  double time_dif_secs = (double) (end - start) / CLOCKS_PER_SEC;
+  //return time in us
+  return (unsigned int) (time_dif_secs);
+}
