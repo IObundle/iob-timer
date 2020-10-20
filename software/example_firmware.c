745eca65a8c96e3c2f41d33c386f58926f4581ef
Jose T. de Sousa <jose.t.de.sousa@gmail.com>
2020-10-20 10:50:29 +0100

    improve example firmware

--- software/example_firmware.c
+++ software/example_firmware.c
@@ -9,18 +9,19 @@ int main()
   unsigned long long elapsed;
   unsigned int elapsedu;
 
-  //read current timer count, compute elapsed time
-  elapsed  = timer_get_count(TIMER_BASE);
-  elapsedu = timer_time_us(TIMER_BASE);
-
-  //init uart 
+  //init timer and uart
+  timer_init(TIMER_BASE);
   uart_init(UART_BASE, FREQ/BAUD);
 
   uart_printf("\nHello world!\n");
   
   uart_txwait();
 
-  uart_printf("\nExecution time: %d clocks in %dus @%dMHz\n\n", 
+  //read current timer count, compute elapsed time
+  elapsed  = timer_get_count(TIMER_BASE);
+  elapsedu = timer_time_us(TIMER_BASE);
+
+  uart_printf("\nExecution time: %d clock cycles = %dus @%dMHz\n\n", 
               (unsigned int)elapsed, elapsedu, FREQ/1000000);
 
   uart_txwait();
