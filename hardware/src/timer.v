2a1931805e47f5fa77c1492110ea23981900c04b
AndreMerendeira <andre.merendeira@gmail.com>
2021-03-03 16:32:50 +0000

    Updated LIB submodule. Changed hw files to account for the changes introduced with the LIB update

--- hardware/src/timer.v
+++ hardware/src/timer.v
@@ -1,14 +1,14 @@
 `timescale 1ns/1ps
 `include "iob_lib.vh"
-`include "TIMERsw_reg_w.vh"
+`include "TIMERsw_reg.vh"
 
 module timer_core
   #(
     parameter DATA_W = 32
     )
    (
-    `INPUT(TIMER_ENABLE, `TIMER_ENABLE_W),    
-    `INPUT(TIMER_SAMPLE, `TIMER_SAMPLE_W),    
+    `INPUT(TIMER_ENABLE, `TIMER_ENABLE_W),
+    `INPUT(TIMER_SAMPLE, `TIMER_SAMPLE_W),
     `OUTPUT(TIMER_VALUE, `TIMER_DATA_LOW_W+`TIMER_DATA_HIGH_W),
     `INPUT(clk, 1),
     `INPUT(rst, 1)
@@ -20,11 +20,10 @@ module timer_core
 
    //time counter register
    `SIGNAL(counter_reg, 2*DATA_W)
-   
+
    `REG_E(clk, TIMER_SAMPLE, counter_reg, time_counter)
    //always @(negedge clk) if(TIMER_SAMPLE)  counter_reg <= time_counter;
 
    `SIGNAL2OUT(TIMER_VALUE, counter_reg)
-      
-endmodule
 
+endmodule
