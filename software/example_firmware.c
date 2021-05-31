31097ed58fa4166dba41aa2748af7a3b59df661b
P_Miranda <pedro_miranda2@hotmail.com>
2021-05-31 13:12:24 +0100

    Updated example firmware for new printf function

--- software/example_firmware.c
+++ software/example_firmware.c
@@ -1,6 +1,7 @@
 #include "system.h"
 #include "periphs.h"
 #include "iob-uart.h"
+#include "printf.h"
 #include "iob_timer.h"
 
 int main()
@@ -12,14 +13,14 @@ int main()
   timer_init(TIMER_BASE);
   uart_init(UART_BASE, FREQ/BAUD);
 
-  uart_printf("\nHello world!\n");
+  printf("\nHello world!\n");
 
   //read current timer count, compute elapsed time
   elapsed  = timer_get_count();
   elapsedu = timer_time_us();
 
-  uart_printf("\nExecution time: %d clock cycles\n", (unsigned int) elapsed);
-  uart_printf("\nExecution time: %dus @%dMHz\n\n", elapsedu, FREQ/1000000);
+  printf("\nExecution time: %d clock cycles\n", (unsigned int) elapsed);
+  printf("\nExecution time: %dus @%dMHz\n\n", elapsedu, FREQ/1000000);
 
   uart_finish();
 }
