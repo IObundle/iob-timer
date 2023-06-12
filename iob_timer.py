4ba7eeab53067957a15d3c06d61323ffe3192e7c
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-06-12 04:51:53 +0100

    feat(python): Add class for object-oriented setup

---
+++ iob_timer.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from iob_module import iob_module
+from setup import setup
+
+# Submodules
+from iob_lib import iob_lib
+from iob_utils import iob_utils
+from iob_clkenrst_portmap import iob_clkenrst_portmap
+from iob_clkenrst_port import iob_clkenrst_port
+from iob_reg import iob_reg
+from iob_reg_e import iob_reg_e
+from iob_reg_r import iob_reg_r
+from iob_counter import iob_counter
+
+class iob_timer(iob_module):
+    name='iob_timer'
+    version = "V0.10"
+    flows = "sim emb"
+    setup_dir = os.path.dirname(__file__)
+
+    @classmethod
+    def _run_setup(cls):
+        # Hardware headers & modules
+        iob_module.generate("iob_s_port")
+        iob_module.generate("iob_s_portmap")
+        iob_lib.setup()
+        iob_utils.setup()
+        iob_clkenrst_portmap.setup()
+        iob_clkenrst_port.setup()
+        iob_reg.setup()
+        iob_reg_e.setup()
+        iob_reg_r.setup()
+        iob_counter.setup()
+
+        cls._setup_confs()
+        cls._setup_ios()
+        cls._setup_regs()
+        cls._setup_block_groups()
+
+        # Verilog modules instances
+        # TODO
+
+        # Copy sources of this module to the build directory
+        super()._run_setup()
+
+        # Setup core using LIB function
+        setup(cls)
+
+
+    @classmethod
+    def _setup_confs(cls):
+        super()._setup_confs([
+            # Macros
+
+            # Parameters
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "`IOB_TIMER_SWREG_ADDR_W",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Address bus width",
+            },
+            {"name": "WDATA_W", "type": "P", "val": "1", "min": "NA", "max": "8", "descr": ""},
+        ])
+
+
+    @classmethod
+    def _setup_ios(cls):
+        cls.ios += [
+            {"name": "iob_s_port", "descr": "CPU native interface", "ports": []},
+            {
+                "name": "general",
+                "descr": "General interface signals",
+                "ports": [
+                    {
+                        "name": "clk_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System clock input",
+                    },
+                    {
+                        "name": "arst_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System reset, asynchronous and active high",
+                    },
+                    {
+                        "name": "cke_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System reset, asynchronous and active high",
+                    },
+                ],
+            },
+        ]
+
+
+    @classmethod
+    def _setup_regs(cls):
+        cls.regs += [
+            {
+                "name": "timer",
+                "descr": "TIMER software accessible registers.",
+                "regs": [
+                    {
+                        "name": "RESET",
+                        "type": "W",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "Timer soft reset",
+                    },
+                    {
+                        "name": "ENABLE",
+                        "type": "W",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "Timer enable",
+                    },
+                    {
+                        "name": "SAMPLE",
+                        "type": "W",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "Sample time counter value into a readable register",
+                    },
+                    {
+                        "name": "DATA_HIGH",
+                        "type": "R",
+                        "n_bits": 32,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "High part of the timer value, which has twice the width of the data word width",
+                    },
+                    {
+                        "name": "DATA_LOW",
+                        "type": "R",
+                        "n_bits": 32,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": True,
+                        "descr": "Low part of the timer value, which has twice the width of the data word width",
+                    },
+                ],
+            }
+        ]
+
+
+    @classmethod
+    def _setup_block_groups(cls):
+        cls.block_groups += []
